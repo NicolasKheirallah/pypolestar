bcf077248b6a72d0172efa044dc7579dd7e65128
Jakob Schlyter <jakob@kirei.se>
2026-05-25 16:39:14 +0200

    Add Wireless

--- pypolestar/grpc_models.py
+++ pypolestar/grpc_models.py
@@ -10,6 +10,7 @@ class ChargingType(StrEnumOptional):
     CHARGING_TYPE_UNSPECIFIED = "Unspecified"
     CHARGING_TYPE_AC = "AC"
     CHARGING_TYPE_DC = "DC"
+    CHARGING_TYPE_WIRELESS = "Wireless"
 
 
 class ChargeTargetLevelSettingType(StrEnumOptional):
